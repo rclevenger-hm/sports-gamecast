d2911ad7d31a8bfd94ae78032d2a003a513c8f0e
rclevenger-hm <70983614+rclevenger-hm@users.noreply.github.com>
2026-09-06 08:15:42 -0500

    feat: add bounded offline app shell cache

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='gamecast-shell-v1';
+const SHELL=['./','./index.html','./gamecast.html','./ticker.html','./gamebar.html','./favorites.js','./notifications.js','./sport-visuals.js','./listen-live.js','./manifest.webmanifest','./app-icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
+});
